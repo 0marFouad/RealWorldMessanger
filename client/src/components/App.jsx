c152e97da658f75bd100fded5a13cde12c9f90c8
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2019-09-13 22:54:40 +0200

    Setup API service

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import api from '../services/api';
 
-const App = function(){
-    return <div>App works</div>
+class App extends React.Component{
+    //runs after rendering
+    async componentDidMount(){
+        const result = await api.call('post', 'auth/login', {
+            username: 'OmarFouad',
+            password: 'password'
+        });
+
+        console.log(result);
+    }
+
+    render(){
+        return <div>Omar Dah sha8al</div>;
+    }
 }
 
 export default App;
